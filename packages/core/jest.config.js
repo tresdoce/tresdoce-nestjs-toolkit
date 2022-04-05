47ce1427b06ce81f907809d743f072bbfd2874fa
Maximiliano Delgado <mdelgado@tresdoce.com.ar>
2022-04-04 22:56:47 -0300

    chore: edit templates

--- packages/core/jest.config.js
+++
@@ -1,6 +0,0 @@
-const config = require('@tresdoce-nestjs-toolkit/config/jest.config');
-
-module.exports = {
-  ...config,
-  rootDir: __dirname,
-};
--- packages/core/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './nestjs-commons';
-export * from './nestjs-https-certificate';
-export * as Typings from './typings';
