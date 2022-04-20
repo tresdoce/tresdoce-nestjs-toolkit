df3a1dd5830c8d0bb4c6448f6568322a23cb2dfd
Maximiliano Delgado <mdelgado@tresdoce.com.ar>
2022-04-19 23:20:52 -0300

    chore: work with version of dev dependencies plop template

--- plopfile.js
+++ plopfile.js
@@ -1,7 +1,15 @@
-const { packageTemplate } = require('./plops');
+const {packageTemplate} = require('./plops');
 
 const config = (plop) => {
-  plop.setGenerator('Package template', packageTemplate);
+    plop.setGenerator('Package template', packageTemplate);
+    plop.setHelper('configVersion', () => {
+        const pkgJson = require('./packages/config/package.json');
+        return pkgJson.version;
+    });
+    plop.setHelper('typesVersion', () => {
+        const pkgJson = require('./packages/tresdoce-types/package.json');
+        return pkgJson.version;
+    });
 };
 
 module.exports = config;
