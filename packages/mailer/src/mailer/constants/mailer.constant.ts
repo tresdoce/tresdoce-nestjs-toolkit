e883883a44ce4a6f93ce835bde9517d37ce894e7
Maximiliano Delgado <mdelgado@tresdoce.com.ar>
2022-12-19 21:00:07 -0300

    fix(mailer): secure smtp

--- packages/mailer/src/mailer/constants/mailer.constant.ts
+++ packages/mailer/src/mailer/constants/mailer.constant.ts
@@ -1,2 +1,6 @@
 export const MAILER_OPTIONS = 'MAILER_OPTIONS';
 export const MAILER_TRANSPORT_FACTORY = 'MAILER_TRANSPORT_FACTORY';
+export const defaultTransportOptions = {
+  secure: true,
+  requireTLS: true,
+};
