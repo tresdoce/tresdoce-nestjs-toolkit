73e3a5d5458dbbb31a778091213236d00e097f9d
Maximiliano Delgado <mdelgado@tresdoce.com.ar>
2023-01-23 21:22:54 -0300

    fix(httpClient): remove export axios

--- packages/http-client/src/index.ts
+++ packages/http-client/src/index.ts
@@ -2,4 +2,3 @@ export { HttpClientModule } from './http/httpClient.module';
 export { HttpClientService } from './http/services/httpClient.service';
 export { HttpClientInterceptor } from './http/interceptors/httpClient.interceptor';
 export * from './http/interfaces/http-module.interface';
-export * from 'axios/index';
