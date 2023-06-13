5353ef2d6a2b907299ac68bfd828aaa5904b1d96
Maximiliano Delgado <mdelgado@tresdoce.com.ar>
2023-06-13 18:11:20 -0300

    feat(paas): add tracing module

--- packages/tracing/src/tracing/constants/tracing.constant.ts
+++ packages/tracing/src/tracing/constants/tracing.constant.ts
@@ -3,11 +3,11 @@ import * as Tags from './tags.constant';
 
 export const defaultExcludePaths = excludePaths();
 export const DEFAULT_TIMEZONE = 'America/Argentina/Buenos_Aires';
-/*
-'dd/LL/yyyy TT.SSS'; // 05/06/2023 16:54:30.752
-'dd/LL/yyyy hh:mm:ss.SSS a'; // 05/06/2023 04:49:44.033 PM
-*/
-export const DEFAULT_TIME_FORMAT = 'dd/LL/yyyy TT.SSS'; // 05/06/2023 16:54:30.752
+
+/** 'dd/LL/yyyy TT.SSS'; // 05/06/2023 16:54:30.752 */
+/** 'dd/LL/yyyy hh:mm:ss.SSS a'; // 05/06/2023 04:49:44.033 PM */
+/** 05/06/2023 16:54:30.752 */
+export const DEFAULT_TIME_FORMAT = 'dd/LL/yyyy TT.SSS';
 
 export const FORMAT_HTTP_HEADERS = 'http_headers';
 export const SPAN_ERROR = 'SPAN_ERROR';
