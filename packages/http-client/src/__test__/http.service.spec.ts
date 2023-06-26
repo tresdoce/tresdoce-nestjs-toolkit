aadf58c3165a38ef8f0ced352ec7c8d799907281
Maximiliano Delgado <mdelgado@tresdoce.com.ar>
2023-06-26 18:42:51 -0300

    test(http-client): resolve problem with test api url

--- packages/http-client/src/__test__/http.service.spec.ts
+++ packages/http-client/src/__test__/http.service.spec.ts
@@ -6,14 +6,8 @@ import { HttpClientService } from '../http/services/httpClient.service';
 import { HttpClientModule } from '../http/httpClient.module';
 import { config } from './utils';
 
-const API_NESTJS_STARTER = 'https://jsonplaceholder.typicode.com';
-
-const mockRequestBody = {
-  userId: 1,
-  id: 1,
-  title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
-  body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
-};
+const API_NESTJS_STARTER = 'https://json-server.up.railway.app/api';
+//const API_NESTJS_STARTER = 'http://localhost:6767/api';
 
 describe('HttpService', () => {
   let app: INestApplication;
@@ -51,11 +45,18 @@ describe('HttpService', () => {
 
   it('should be return status 200 - request', async () => {
     const { status, data } = await service.request({
-      url: `${API_NESTJS_STARTER}/posts`,
+      headers: {
+        'uber-trace-id': 'ef083b253ad4792827d19be437195454:3c5bd7e941c53cfc:0:01',
+        'x-amzn-trace-id':
+          'Root=1-ef083b25-3ad4792827d19be437195454;Parent=3c5bd7e941c53cfc;Sampled=1',
+        my_header: 'test-header',
+      },
+      url: `${API_NESTJS_STARTER}/users?limit=5`,
       method: 'GET',
     });
     expect(status).toBe(200);
     expect(data.length).toBeGreaterThan(0);
+    expect(data.length).toEqual(5);
   });
 
   it('should be return status 200 - get', async () => {
@@ -65,54 +66,72 @@ describe('HttpService', () => {
   });
 
   it('should be return status 201 - post', async () => {
-    const { status, data } = await service.post(`${API_NESTJS_STARTER}/posts`, {
-      data: mockRequestBody,
+    const { status, data } = await service.post(`${API_NESTJS_STARTER}/users`, {
+      data: {
+        email: 'test@email.com',
+      },
     });
     expect(status).toBe(201);
     expect(data).toEqual({
-      ...mockRequestBody,
-      id: 101,
+      success: true,
+      data: {
+        id: 25,
+        email: 'test@email.com',
+      },
     });
   });
 
   it('should be return status 200 - get by id', async () => {
-    const { status, data } = await service.get(`${API_NESTJS_STARTER}/posts/1`);
+    const { status, data } = await service.get(`${API_NESTJS_STARTER}/users/1`);
     expect(status).toBe(200);
-    expect(data).toEqual(mockRequestBody);
+    expect(data).toBeDefined();
+    expect(data).not.toBeEmpty();
   });
 
   it('should be return status 200 - put', async () => {
-    const { status, data } = await service.put(`${API_NESTJS_STARTER}/posts/1`, {
+    const { status, data } = await service.put(`${API_NESTJS_STARTER}/users/1`, {
       data: {
-        ...mockRequestBody,
-        title: 'mockito',
+        email: 'newtest@email.com',
       },
     });
     expect(status).toBe(200);
-    expect(data).toEqual({ ...mockRequestBody, id: 1, title: 'mockito' });
+    expect(data).toEqual({
+      success: true,
+      data: {
+        id: 1,
+        email: 'newtest@email.com',
+      },
+    });
   });
 
   it('should be return status 200 - delete', async () => {
-    const { status, data } = await service.delete(`${API_NESTJS_STARTER}/posts/2`);
+    const { status, data } = await service.delete(`${API_NESTJS_STARTER}/users/2`);
     expect(status).toBe(200);
-    expect(data).toEqual({});
+    expect(data).toEqual({
+      success: true,
+    });
   });
 
   it('should be return status 200 - patch', async () => {
-    const { status, data } = await service.patch(`${API_NESTJS_STARTER}/posts/1`, {
+    const { status, data } = await service.patch(`${API_NESTJS_STARTER}/users/1`, {
       data: {
-        ...mockRequestBody,
-        title: 'mockito',
+        email: 'newtest@email.com',
       },
     });
     expect(status).toBe(200);
-    expect(data).toEqual({ ...mockRequestBody, id: 1, title: 'mockito' });
+    expect(data).toEqual({
+      success: true,
+      data: {
+        id: 1,
+        email: 'newemailtest@email.com',
+      },
+    });
   });
 
   it('should be return status 200 - head', async () => {
     const { status, headers } = await service.head(`${API_NESTJS_STARTER}/posts`);
     expect(status).toBe(200);
-    expect(headers).toHaveProperty('etag');
+    expect(headers).toHaveProperty('content-type');
   });
 
   it('should be return error', async () => {
