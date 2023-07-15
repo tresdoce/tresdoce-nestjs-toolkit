12ba37e2bd8e6d8e45d46772b1e3c8b3200becf8
Maximiliano Delgado <mdelgado@tresdoce.com.ar>
2023-07-15 18:12:42 -0300

    refactor(commons): change build webpack config

--- packages/commons/src/__test__/build-config.spec.ts
+++ packages/commons/src/__test__/build-config.spec.ts
@@ -1,4 +1,6 @@
 import { buildConfig } from '../index';
+import { Configuration } from '@nestjs/cli/lib/configuration';
+
 describe('build-config', () => {
   const INIT_ENVS = process.env;
 
@@ -11,9 +13,9 @@ describe('build-config', () => {
     process.env = INIT_ENVS;
   });
 
-  it('should be return webpack config not is build', () => {
+  it('should return the development configuration when NODE_ENV is not "build"', () => {
     process.env.NODE_ENV = 'dev';
-    const config = buildConfig({ output: {}, optimization: {} }, {});
+    const config = buildConfig() as unknown as Configuration;
     expect(config).not.toBe(null);
     expect(typeof config).toBe('object');
     expect(config).toBeDefined();
@@ -21,13 +23,24 @@ describe('build-config', () => {
     expect(config.optimization.nodeEnv).toBeFalsy();
   });
 
-  it('should be return webpack config is build', () => {
+  it('should return the production configuration when NODE_ENV is "build"', () => {
     process.env.NODE_ENV = 'build';
-    const config = buildConfig({ output: {}, optimization: {} }, {});
+    const config = buildConfig() as unknown as Configuration;
     expect(config).not.toBe(null);
     expect(typeof config).toBe('object');
     expect(config).toBeDefined();
     expect(config.mode).toEqual('production');
     expect(config.optimization.nodeEnv).toEqual('production');
+  });
+
+  it('should merge with the additional config', () => {
+    const additionalConfig = { devtool: 'eval' };
+    const config = buildConfig(additionalConfig) as unknown as Configuration;
+    expect(config).not.toBe(null);
+    expect(typeof config).toBe('object');
+    expect(config).toBeDefined();
+    expect(config.mode).toEqual('development');
+    expect(config.devtool).toEqual('eval');
+    expect(config.optimization.nodeEnv).toBeFalsy();
   });
 });
--- packages/commons/src/build-config/index.ts
+++ packages/commons/src/build-config/index.ts
@@ -1,46 +1,56 @@
+import { merge } from 'webpack-merge';
 const nodeExternals = require('webpack-node-externals');
 const TerserPlugin = require('terser-webpack-plugin');
 
-export const buildConfig = (options, newConfig) => {
-  const isProductionMode = process.env.NODE_ENV === 'build';
-  return {
-    ...options,
-    entry: './src/main.ts',
-    mode: isProductionMode ? 'production' : 'development',
-    target: 'node',
-    externals: [nodeExternals()],
-    output: {
-      ...options.output,
-      filename: 'main.js',
-    },
-    resolve: {
-      extensions: ['.ts', '.js'],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.ts$/,
-          exclude: /node_modules/,
-          use: ['ts-loader'],
+const commonConfig = {
+  entry: './src/main.ts',
+  target: 'node',
+  externals: [nodeExternals()],
+  output: {
+    filename: 'main.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: ['ts-loader'],
+      },
+    ],
+  },
+};
+
+const devConfig = {
+  mode: 'development',
+  devtool: 'source-map',
+  optimization: {
+    minimize: false,
+    nodeEnv: false,
+  },
+};
+
+const prodConfig = {
+  mode: 'production',
+  devtool: 'hidden-source-map',
+  optimization: {
+    minimize: true,
+    nodeEnv: 'production',
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          keep_classnames: true,
+          keep_fnames: true,
         },
-      ],
-    },
-    optimization: {
-      ...options.optimization,
-      minimize: isProductionMode,
-      minimizer: isProductionMode
-        ? [
-            new TerserPlugin({
-              terserOptions: {
-                keep_classnames: true,
-                keep_fnames: true,
-              },
-            }),
-          ]
-        : [],
-      nodeEnv: isProductionMode ? 'production' : false,
-    },
-    devtool: isProductionMode ? 'hidden-source-map' : 'source-map',
-    ...newConfig,
-  };
+      }),
+    ],
+  },
+};
+
+export const buildConfig = (additionalConfig = {}) => {
+  const isBuildMode = process.env.NODE_ENV === 'build';
+  const specificConfig = isBuildMode ? prodConfig : devConfig;
+  return merge(commonConfig, specificConfig, additionalConfig);
 };
