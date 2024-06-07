1559b5f268cedce98b0331be8af30be2fa915559
Maximiliano Delgado <mdelgado@tresdoce.com.ar>
2024-06-07 11:50:36 -0300

    feat(health): expose liveness path

--- packages/health/src/health/controllers/liveness.controller.ts
+++ packages/health/src/health/controllers/liveness.controller.ts
@@ -1,12 +1,36 @@
 import { Controller, Get } from '@nestjs/common';
-import { ApiExcludeEndpoint } from '@nestjs/swagger';
+import { ApiExcludeEndpoint, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { SkipTrace } from '@tresdoce-nestjs-toolkit/tracing';
+
+import { LivenessResponse } from '../entities/liveness.entity';
+import { Public } from '@tresdoce-nestjs-toolkit/core';
 
 @Controller('health')
 export class LivenessController {
   @Get('liveness')
-  @ApiExcludeEndpoint()
+  @Public()
   @SkipTrace()
+  @ApiTags('Monitoring')
+  @ApiOperation({
+    summary: 'Liveness check',
+    description:
+      'This endpoint returns the liveness status of the service. It is used to check if the service is alive.',
+    operationId: 'checkLiveness',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'The service is live.',
+    type: LivenessResponse,
+    schema: {
+      example: {
+        status: 'up',
+      },
+    },
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal server error.',
+  })
   getLiveness() {
     return { status: 'up' };
   }
