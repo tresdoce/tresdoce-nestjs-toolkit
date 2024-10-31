5b833906f67a288ce89d2e7d248f212b0662e5dc
Maximiliano Delgado <mdelgado@tresdoce.com.ar>
2024-10-31 17:02:29 -0300

    feat: add template dynamic module

--- plopfile.js
+++ plopfile.js
@@ -1,7 +1,8 @@
-const { packageTemplate } = require('./plops');
+const { dynamicModuleTemplate, basicPackageTemplate } = require('./plops');
 
 const config = (plop) => {
-    plop.setGenerator('Package template', packageTemplate);
+    plop.setGenerator('Dynamic Module template', dynamicModuleTemplate);
+    plop.setGenerator('Basic package template', basicPackageTemplate);
     plop.setHelper('configVersion', () => require('./packages/config/package.json').version);
     plop.setHelper('typesVersion', () => require('./packages/tresdoce-types/package.json').version);
 };
